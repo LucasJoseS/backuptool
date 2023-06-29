3a825e70d9e50cfb4b0ac017757cdeab2e98b820
LucasJSilva <lucasjsilva_0xf0@protonmail.com>
2023-06-28 21:29:26 -0300

    Add suport to mutiples path

--- backuptool.cpp
+++ backuptool.cpp
@@ -19,7 +19,7 @@ class BackupTarget {
 
     std::tm* current_tm = std::localtime(&current_time_t);
     int year  = current_tm->tm_year + 1900;
-    int month = current_tm->tm_mon;
+    int month = current_tm->tm_mon  + 1;
 
     auto destination_path = backup_root / std::to_string(year)
       / std::to_string(month)
@@ -27,7 +27,12 @@ class BackupTarget {
       / path.filename();
 
     std::filesystem::create_directories(destination_path);
-    std::filesystem::copy(path, destination_path, std::filesystem::copy_options::recursive);
+
+    const auto options = std::filesystem::copy_options::skip_existing
+                       | std::filesystem::copy_options::recursive
+                       ;
+
+    std::filesystem::copy(path, destination_path, options);
   }
 };
 
@@ -40,15 +45,16 @@ int main() {
   auto general          = config["general"];
   auto target_categorys = config["backup"];
 
-  std::string backup_root_path_string     = general["backup-root-path"].as<std::string>();
-  std::filesystem::path backup_root_path  = std::filesystem::path(backup_root_path_string);
-  for (const auto target : target_categorys) {
-    std::string category       = target.first.as<std::string>();
-    std::string path_string    = target.second.as<std::string>();
+  std::filesystem::path backup_root_path(general["backup-root-path"].as<std::string>());
+  for(auto target: target_categorys) {
+    std::stringstream paths(target.second.as<std::string>());
+    std::string path_s;
 
-    std::filesystem::path path = std::filesystem::path(path_string);
+    while(paths >> path_s) {
+      std::filesystem::path path(path_s);
 
-    BackupTarget backupTarget(path, category);
-    backupTarget.backup(backup_root_path);
+      BackupTarget backup(path, target.first.as<std::string>());
+      backup.backup(backup_root_path);
+    }
   }
 }
