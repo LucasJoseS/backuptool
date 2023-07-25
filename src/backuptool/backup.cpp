302203dba853932d2c630e5c3b3a8b96dd40d596
LucasJSilva <lucasjsilva_0xf0@protonmail.com>
2023-07-25 20:33:26 -0300

    clean code

--- src/backuptool/backup.cpp
+++ src/backuptool/backup.cpp
@@ -1,5 +1,9 @@
 #include <backuptool.hpp>
 
+#define OPTIONS                                                                \
+  std::filesystem::copy_options::skip_existing |                               \
+      std::filesystem::copy_options::recursive
+
 std::tm *currentLocaltime() {
   auto current_time = std::chrono::system_clock::now();
   std::time_t current_time_t =
@@ -8,23 +12,22 @@ std::tm *currentLocaltime() {
   return std::localtime(&current_time_t);
 }
 
+std::filesystem::path destinationPath(std::tm *time, target target,
+                                      filesystem::path backup_root_path) {
+  int year = time->tm_year + 1900;
+  int month = time->tm_mon + 1;
+  int day = time->tm_mday;
+
+  return backup_root_path / std::to_string(year) / std::to_string(month) /
+         std::to_string(day) / target.category / target.path.filename();
+}
+
 int backuptool::backup::simpleBackup(target target,
                                      filesystem::path backup_root_path) {
-  auto current_tm = currentLocaltime();
-
-  int year = current_tm->tm_year + 1900;
-  int month = current_tm->tm_mon + 1;
-  int day = current_tm->tm_mday;
-
-  auto destination_path = backup_root_path / std::to_string(year) /
-                          std::to_string(month) / std::to_string(day) /
-                          target.category / target.path.filename();
+  auto time = currentLocaltime();
+  auto destination_path = destinationPath(time, target, backup_root_path);
 
   std::filesystem::create_directories(destination_path);
-
-  const auto options = std::filesystem::copy_options::skip_existing |
-                       std::filesystem::copy_options::recursive;
-
-  std::filesystem::copy(target.path, destination_path, options);
+  std::filesystem::copy(target.path, destination_path, OPTIONS);
   return true;
 }
